4c9fc4b61b9e7c8fd3188c47a4a449da2b61a863
robin <re@incubo.ca>
2016-12-07 23:10:03 -0500

    added simple form validation

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,9 +4,15 @@ module.exports = (app) => {
 
   app.post('/login', (req, res) => {
     const user = req.body.user
-    res.render('index', {
-      loginEmail: user.email,
-      loginPassword: user.password
-    });
+    if (user.password === '' || user.email === '') {
+      res.render('login', {
+        error: 'you are missing some info'
+      });
+    }else {
+      res.render('index', {
+        loginEmail: user.email,
+        loginPassword: user.password
+      });
+    }
   });
 }

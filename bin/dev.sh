92c6571283d549e5674b4c8fe7e357769403c29c
robin <re@incubo.ca>
2016-12-08 12:34:40 -0500

    removed nodemon from dev.sh as it was not working

--- bin/dev.sh
+++ bin/dev.sh
@@ -2,18 +2,9 @@
 
 export NODE_PATH=src/
 
-DEV_CMD="nodemon \
-  --ignore bin \
-  --ignore public \
-  --ignore node_modules \
-  --ignore build \
-  --ignore webpack.config.json"
+DEV_SERVER_CMD="babel-node --harmony src/app.js"
 
-DEV_SERVER_CMD="$DEV_CMD --exec babel-node --harmony src/app.js"
-
-WEBPACK_CMD="nodemon \
-  --watch config/webpack.config.js \
-  --exec 'webpack --config config/webpack.config.js --watch'"
+WEBPACK_CMD="webpack --config config/webpack.config.js --watch"
 
 LINT_CMD="esw \
   --config config/.eslintrc.json \
